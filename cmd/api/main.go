f259476ec31ac1bcfd4ffa882b62c8226641304a
nullsploit01 <harshaldharmik099@gmail.com>
2024-07-29 21:40:15 +0530

    add init log

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"iosync/internal/server"
+	"log"
 )
 
 func main() {
 
 	server := server.NewServer()
+
+	log.Println("Started Server on address", server.Addr)
 
 	err := server.ListenAndServe()
 	if err != nil {
