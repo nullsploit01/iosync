2836469447fc0a533386b68bfd1354c97e7f0ccf
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-05 03:18:17 +0530

    add validation for login

--- internal/server/auth.go
+++ internal/server/auth.go
@@ -10,7 +10,24 @@ type RegisterRequest struct {
 	Password string `json:"password" validate:"required"`
 }
 
+type LoginRequest struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
 func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
+	var request LoginRequest
+
+	if err := s.readJson(w, r, &request); err != nil {
+		s.errorJson(w, err)
+		return
+	}
+
+	if err := validateInput(&request); err != nil {
+		s.errorJson(w, err, http.StatusBadRequest)
+		return
+	}
+
 	response := Response{
 		Message: "Login",
 	}
