ec9e725b32073907d749a506f14c6a2fbbef935f
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-04 22:44:10 +0530

    install ent and add user basic schema

---
+++ ent/schema/user.go
@@ -0,0 +1,39 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("username").Unique(),
+		field.String("password"),
+		field.String("name"),
+		field.Bool("is_active").Default(true),
+		field.Time("created_at").Default(time.Now()),
+		field.Time("updated_at").Default(time.Now()),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
+
+// Index of the User.
+
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("username"),
+	}
+}
