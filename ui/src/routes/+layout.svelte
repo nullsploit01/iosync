fb8a39a2a815c7c2476967bdbb28c3b7b8484770
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-12 02:47:27 +0530

    add ability to call login api

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css'
 </script>
 
-<div class="p-2 h-full">
+<div class="p-2 h-full bg-gray-200">
   <slot></slot>
 </div>
--- ui/src/routes/auth/login/+page.svelte
+++ ui/src/routes/auth/login/+page.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
-    import { Button } from "@/components/ui/button";
-  import {Input} from "@/components/ui/input"
+  import { Button } from '@/components/ui/button'
+  import { Input } from '@/components/ui/input'
+  import { API_BASE_URL } from '@/config'
+  import { writable } from 'svelte/store'
+
+  let username = writable('')
+  let password = writable('')
+
+  async function handleSubmit() {
+    let usernameValue, passwordValue
+
+    // Subscribe to the writable stores to get the values
+    username.subscribe((value) => (usernameValue = value))()
+    password.subscribe((value) => (passwordValue = value))()
+
+    const loginData = {
+      username: usernameValue,
+      password: passwordValue
+    }
+    const response = await fetch(`${API_BASE_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginData)
+    })
+
+    if (!response.ok) {
+      throw new Error('Login failed')
+    }
+
+    const data = await response.json()
+    console.log('Login successful:', data)
+  }
 </script>
 
-<div class="flex place-items-center justify-center h-full">
-    <div class="flex flex-col justify-center place-items-center">
-        <Input placeholder="Username" class="m-3" />
-        <Input placeholder="Password" class="m-3"  />
-        <Button class="m-3">Login</Button>
-    </div>
+<div class="flex h-full place-items-center justify-center">
+  <div class="flex flex-col place-items-center justify-center">
+    <Input bind:value={$username} placeholder="Username" class="m-3 border-gray-400" />
+    <Input
+      bind:value={$password}
+      type="password"
+      placeholder="Password"
+      class="m-3 border-gray-400"
+    />
+    <Button size="lg" class="m-3" on:click={handleSubmit}>Login</Button>
+  </div>
 </div>
