c8fccc91f8c1eada0316b13d7f98eab1b5445c7b
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-13 23:39:21 +0530

    add error interceptor for axios

--- ui/src/lib/clients/http/index.ts
+++ ui/src/lib/clients/http/index.ts
@@ -1,7 +1,30 @@
-import { API_BASE_URL } from "@/config/environment";
-import axios from "axios";
+import { goto } from '$app/navigation'
+import { API_BASE_URL } from '@/config/environment'
+import axios, { AxiosError, HttpStatusCode, type AxiosResponse } from 'axios'
 
-export const httpClient = axios.create({
-    baseURL: API_BASE_URL,
-    withCredentials: true
-}) 
+const httpClient = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true
+})
+
+const responseInterceptor = (response: AxiosResponse) => {
+  return response
+}
+
+const errorInterceptor = (error: AxiosError) => {
+  switch (error.response?.status) {
+    case HttpStatusCode.Unauthorized: {
+      goto('/auth/login')
+      break
+    }
+
+    case HttpStatusCode.NotFound: {
+      break
+    }
+  }
+
+  return Promise.reject(error)
+}
+
+httpClient.interceptors.response.use(responseInterceptor, errorInterceptor)
+export { httpClient }
