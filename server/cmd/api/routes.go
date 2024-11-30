0cb0630fd05bd33290e34e63c47c74ce069233c5
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-30 23:17:51 +0530

    add heartbeat and cors middleware

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
@@ -16,6 +17,17 @@ func (app *application) routes() http.Handler {
 	mux.Use(app.logAccess)
 	mux.Use(app.recoverPanic)
 	mux.Use(middleware.RedirectSlashes)
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	mux.Get("/status", app.status)
 
