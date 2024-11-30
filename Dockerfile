d8164bc7cae68ec0e5b12f617bd4e6fe308dd486
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-30 05:35:57 +0530

    remove extra stuff

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM golang:1.22.2
-
-WORKDIR /go/src/app
-
-COPY . .
-
-RUN go build -o main ./cmd/api
-
-CMD ["./main"]
--- pkg/constants/cookies.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-type ContextKey string
-
-const SessionIDKey ContextKey = "session_id"
-
-const UsernameKey ContextKey = "user_id"
