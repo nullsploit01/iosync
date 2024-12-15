95ab8b4855d7a823d8089a1e12c7b3d8f7c5836e
nullsploit01 <harshaldharmik099@gmail.com>
2024-12-15 23:41:25 +0530

    add table for tracking node updates

---
+++ server/ent/schema/nodeupdate.go
@@ -0,0 +1,18 @@
+package schema
+
+import "entgo.io/ent"
+
+// NodeUpdate holds the schema definition for the NodeUpdate entity.
+type NodeUpdate struct {
+	ent.Schema
+}
+
+// Fields of the NodeUpdate.
+func (NodeUpdate) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the NodeUpdate.
+func (NodeUpdate) Edges() []ent.Edge {
+	return nil
+}
