1a026099ef79dde3f7d8940a46a66eb34f527de3
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-06 22:52:51 +0530

    add ability to render nodes

--- client/mobile/src/app/index.tsx
+++ client/mobile/src/app/index.tsx
@@ -1,10 +1,42 @@
-import { Text, View } from 'tamagui'
+import { useEffect, useState } from 'react'
+import { Button, Card, H2, Paragraph, View, XStack } from 'tamagui'
+
+import { nodeAPIService } from '@/src/service/api/node'
+import { INode } from '@/src/types/models/node'
 
 const HomeScreen = () => {
+  const [_nodes, setNodes] = useState<INode[]>([])
+
+  useEffect(() => {
+    fetchNodes()
+  }, [])
+
+  const fetchNodes = async () => {
+    const response = await nodeAPIService.getNodes()
+    setNodes(response.data.data)
+  }
+
   return (
     <View>
-      <Text>HomeScreeenn</Text>
+      {_nodes.map((node) => (
+        <NodeCard key={node.id} node={node} />
+      ))}
     </View>
+  )
+}
+
+const NodeCard = ({ node }: { node: INode }) => {
+  return (
+    <Card elevate size="$4" bordered>
+      <Card.Header padded>
+        <H2>{node.name}</H2>
+        <Paragraph theme="alt2">{node.description}</Paragraph>
+      </Card.Header>
+      <Card.Footer padded>
+        <XStack flex={1} />
+        <Button borderRadius="$10">Purchase</Button>
+      </Card.Footer>
+    </Card>
   )
 }
 
--- client/mobile/src/service/api/node/index.ts
+++ client/mobile/src/service/api/node/index.ts
@@ -1,7 +1,10 @@
 import { httpClient } from '..'
 
+import { IAPIResponse } from '@/src/types/api'
+import { INode } from '@/src/types/models/node'
+
 export const nodeAPIService = {
   getNodes: async () => {
-    return await httpClient.get('/nodes')
+    return await httpClient.get<IAPIResponse<INode[]>>('/nodes')
   }
 }
